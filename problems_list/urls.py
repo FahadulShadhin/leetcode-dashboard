f8d7b27e2d40b6e916fc20c27ee120abc7fa4f4a
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-10-18 20:17:34 +0600

    url patterns restructures

--- problems_list/urls.py
+++ problems_list/urls.py
@@ -3,7 +3,19 @@ from . import views
 
 app_name = 'problems_list'
 urlpatterns = [
-    path('', views.problems_list, name='home'),
-    path('<int:pk>/', views.note, name='note'),
-    path('about/', views.about, name='about')
+    path(
+        '', 
+        views.problems_list, 
+        name='home'
+    ),
+    path(
+        '<int:pk>/', 
+        views.note, 
+        name='note'
+    ),
+    path(
+        'about/', 
+        views.about, 
+        name='about'
+    ),
 ]
