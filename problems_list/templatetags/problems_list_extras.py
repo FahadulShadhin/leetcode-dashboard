45e383834c10dd533e4de9896846c4878c5f56e2
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-10-19 09:56:21 +0600

    templatetags refactor

--- problems_list/templatetags/problems_list_extras.py
+++ problems_list/templatetags/problems_list_extras.py
@@ -6,6 +6,7 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
+@stringfilter
 def convert_markdown(value):
     return markdown.markdown(
         value, 
