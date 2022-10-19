0a783f51e831777282250e5b5462ddd8953309a3
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-10-20 04:38:59 +0600

    model refactor

--- problems_list/models.py
+++ problems_list/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Difficulty(models.Model):
     name = models.CharField(max_length=7, null=True)
 
@@ -8,12 +9,12 @@ class Difficulty(models.Model):
         
 
 class Problem(models.Model):
-    difficulty = models.ForeignKey(Difficulty, on_delete=models.CASCADE)
-    name = models.CharField(max_length=256, null=True)
-    topic = models.CharField(max_length=256, null=True)
-    add_date = models.DateTimeField(auto_now_add=True)
-    link = models.URLField(max_length=256, unique=True, blank=True)
-    note = models.TextField(null=True)
+    difficulty  = models.ForeignKey(Difficulty, on_delete=models.CASCADE)
+    name        = models.CharField(max_length=256, null=True)
+    topic       = models.CharField(max_length=256, null=True)
+    add_date    = models.DateTimeField(auto_now_add=True)
+    link        = models.URLField(max_length=256, unique=True, blank=True)
+    note        = models.TextField(null=True)
     source_code = models.FileField(upload_to='source_code/', null=True, default=None)
 
     def __str__(self):
